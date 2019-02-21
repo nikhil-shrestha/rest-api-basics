d92a8c953770d4daa9bcc010d0993d8c56b014ba
illusionist <nikhil.shrestha1995@gmail.com>
2019-02-21 17:17:29 +0545

    added schema and resolvers for login and create post

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,7 +1,9 @@
 const bcrypt = require("bcryptjs");
 const validator = require("validator");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
+const Post = require("../models/post");
 
 module.exports = {
   createUser: async function({ userInput }, req) {
@@ -36,5 +38,74 @@ module.exports = {
     });
     const createdUser = await user.save();
     return { ...createdUser._doc, _id: createdUser._id.toString() };
+  },
+  login: async function({ email, password }) {
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      const error = new Error("User not found.");
+      error.code = 401;
+      throw error;
+    }
+    const isEqual = await bcrypt.compare(password, user.password);
+    if (!isEqual) {
+      const error = new Error("Password is incorrect.");
+      error.code = 401;
+      throw error;
+    }
+    const token = jwt.sign(
+      {
+        userId: user._id.toString(),
+        email: user.email
+      },
+      "somesupersecret",
+      { expiresIn: "1h" }
+    );
+    return {
+      token: token,
+      userId: user._id.toString()
+    };
+  },
+  createPost: async function({ postInput }, req) {
+    const errors = [];
+    if (
+      validator.isEmpty(postInput.title) ||
+      !validator.isLength(postInput.title, { min: 5 })
+    ) {
+      errors.push({ message: "Title is invalid" });
+    }
+    if (
+      validator.isEmpty(postInput.content) ||
+      !validator.isLength(postInput.content, { min: 5 })
+    ) {
+      errors.push({ message: "Content is invalid" });
+    }
+    if (
+      validator.isEmpty(postInput.imageUrl) ||
+      !validator.isLength(postInput.imageUrl, { min: 5 })
+    ) {
+      errors.push({ message: "ImageUrl is invalid" });
+    }
+    if (errors.length > 0) {
+      const error = new Error("Input Invalid.");
+      error.data = errors;
+      error.code = 422;
+      throw error;
+    }
+
+    const post = new Post({
+      title: postInput.title,
+      content: postInput.content,
+      imageUrl: postInput.imageUrl
+    });
+
+    const createdPost = await post.save();
+    // Add post to users' posts
+
+    return {
+      ...createdPost._doc,
+      id: createdPost._id.toString(),
+      createdAt: createdPost.createdAt.toISOString(),
+      updatedAt: createdPost.updatedAt.toISOString()
+    };
   }
 };
